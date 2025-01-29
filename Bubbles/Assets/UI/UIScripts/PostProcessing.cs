5fdc77ca1b9ffacbe0cc54e0bd3132a0eb98ade7
HvtEnde <97097945@st.deltion.nl>
2025-01-29 14:59:37 +0100

    LensDistortionScript

--- Bubbles/Assets/UI/UIScripts/PostProcessing.cs
+++ Bubbles/Assets/UI/UIScripts/PostProcessing.cs
@@ -1,17 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 
-public class NewBehaviourScript : MonoBehaviour
+public class LensDistortionURP : MonoBehaviour
 {
-    public Volume postProcessing;
-    public LensDistortion distortion;
+    public Volume volume;
+    private LensDistortion lensDistortion;
 
-
-    public float minDistortion = -50f;
-    public float maxDistortion = 50f;
+    public float minDistortion = -0.5f;
+    public float maxDistortion = 0.5f;
     public float speed = 2f;
 
+    private float t = 0f;
+    private bool increasing = true;
+
+    void Start()
+    {
+        if (volume.profile.TryGet(out lensDistortion))
+        {
+            Debug.Log("Lens Distortion gevondenuhuhhhh");
+        }
+        else
+        {
+            Debug.LogError("Lens Distortion niet gevonden in Volume profile idioot");
+        }
+    }
+
+    void Update()
+    {
+        if (lensDistortion == null) return;
+
+        
+        t += (increasing ? 1 : -1) * speed * Time.deltaTime;
+
+        if (t >= 1f)
+        {
+            t = 1f;
+            increasing = false;
+        }
+        else if (t <= 0f)
+        {
+            t = 0f;
+            increasing = true;
+        }
+
+        
+        lensDistortion.intensity.value = Mathf.Lerp(minDistortion, maxDistortion, t);
+    }
 }
